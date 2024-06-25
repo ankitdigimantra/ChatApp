71e891bb65f48fc3dfdcc0ce1f927ef47dad275a
Ankit Singh <ankit.singh@digimantra.com>
2024-06-25 18:30:25 +0530

    first commit

---
+++ src/components/Cat.js
@@ -0,0 +1,12 @@
+import { Avatar } from '@mui/material'
+import React from 'react'
+
+const Cat = () => {
+  return (
+    <div>
+      <Avatar src="https://images.unsplash.com/photo-1511216113906-8f57bb83e776?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
+    </div>
+  )
+}
+
+export default Cat
--- src/layouts/dashboard/index.js
+++ src/layouts/dashboard/index.js
@@ -1,11 +1,36 @@
 import React from "react";
+import { useTheme } from "@mui/material/styles";
 import { Outlet } from "react-router-dom";
+import { Box, Stack } from "@mui/material";
+import Profile from "../../assets/Images/2iN6x15XtxnwJg55oNTe6QKyWut 1 (1).svg";
 
 const DashboardLayout = () => {
+  const theme = useTheme();
 
   return (
     <>
-      Dashboard Layout
+      <Box
+        sx={{
+          backgroundColor: theme.palette.background.paper,
+          height: "100vh",
+          boxShadow: "0px 0px 2px rgba(0,0,0, 0.25)",
+          width: 100,
+        }}
+      >
+        <Stack direction="column" alignItems={"center"} sx={{ width: "100%" }}>
+          <Box
+            sx={{
+              backgroundColor: theme.palette.main,
+              height: 64,
+              width: 64,
+              borderRadius: 1.5,
+            }}
+          >
+            <img src={Profile} alt="Profile" size="48" />
+          </Box>
+          {Nav_Buttons.map((el) => <IconButton key={el.index}>{el.icon}</IconButton>)}
+        </Stack>
+      </Box>
       <Outlet />
     </>
   );
